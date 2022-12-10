e62554c2989e93edf5580849b31dd734ebce52a3
aryandeven180800 <114887600+aryandeven180800@users.noreply.github.com>
2022-12-10 01:32:11 -0500

    commit docor org

---
+++ Aryan Child Adoption/src/aryan/child/adoption/AryanChildAdoption.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aryan.child.adoption;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AryanChildAdoption {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
