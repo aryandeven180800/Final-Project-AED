a9c025bf292a36ebe9ed618d625e5c49688472d8
aryandeven180800 <114887600+aryandeven180800@users.noreply.github.com>
2022-12-11 00:58:06 -0500

    connector

---
+++ src/Business/WorkQueue/FinanceCheckProcessWorkRequest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class FinanceCheckProcessWorkRequest extends WorkRequest {
+
+    private String result;
+    private String financeStatus;
+    private String remarks;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getFinanceStatus() {
+        return financeStatus;
+    }
+
+    public void setFinanceStatus(String financeStatus) {
+        this.financeStatus = financeStatus;
+    }
+
+}
---
+++ src/Business/WorkQueue/MedicalHelpWorkRequest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MedicalHelpWorkRequest extends WorkRequest {
+    
+    private String testResult;
+    private String prescription;
+    private String remark;
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(String prescription) {
+        this.prescription = prescription;
+    }
+
+    public String getTestResult() {
+        return testResult;
+    }
+
+    public void setTestResult(String testResult) {
+        this.testResult = testResult;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+    
+}
